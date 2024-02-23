f1c49d33bcaa49c9459486d5b6fa2d6c7ed5bb6e
Azmat-Yasmin <yasminazmat786@gmail.com>
2024-02-23 10:11:52 -0800

    first commit

--- Problemset8/seasons/shirtificate/shirtificate.py
+++ Problemset8/seasons/shirtificate/shirtificate.py
@@ -1,12 +1,31 @@
 from fpdf import FPDF
 
-name = input('Name: ')
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('helvetica', 'B', 45)
-pdf.cell(0, 60, 'CS50 Shirtificate', align='C')
-pdf.image('shirtificate.png', x= 0, y= 70)
-pdf.set_font_size(30)
-pdf.set_text_color(255, 255, 255)
-pdf.text(x=45, y=150, text=f'{name} took CS50')
-pdf.output('shirtificate.pdf')
+def main():
+    name = input('Name: ')
+
+    # Create a PDF object with A4 format, 'mm' unit, and 'Portrait' orientation
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    # Add header
+    pdf.set_font('helvetica', 'B', 30)
+    pdf.cell(200, 50, 'CS50 Shirtificate', ln=True, align='C')
+
+    # Add shirt image
+    pdf.image('C:\Python(Harvard)\Problemset8\seasons\shirtificate\shortificate.png', x=20, y=80, w=170)
+
+    # Set text color to white for printing inside the shirt
+    pdf.set_text_color(r=255, g=255, b=255)
+
+    # Set position for the text inside the shirt
+    pdf.set_x(20)
+    pdf.set_y(150)
+
+    # Print the name inside the shirt
+    pdf.cell(170, 10, f'{name} took CS50', ln=True, align='C')
+
+    # Save the PDF
+    pdf.output('Text_On_Tshirt.pdf')
+
+if __name__ == '__main__':
+    main()
